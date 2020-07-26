eaeb9be5c1d01aec21db9db68dff60fb228acad0
Chaudhary Sarimurrab <samurrab@gmail.com>
2020-07-26 20:40:49 +0530

    Saving Unique code and Url

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,10 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return 'We can not allow get method for security purpose'
+        return redirect(url_for('home'))
